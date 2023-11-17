61abff4da1634cd06fe260fe942af93c1c24dc31
Daniel Shifman <shifmandaniel@gmail.com>
2023-11-16 21:48:45 -0800

    Added minimal command-line argument support

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "Config.h"
 #include "Devices.h"
 #include "Authenticator.h"
+#include "Dirigera.h"
 
 using namespace std;
+
+vector<string> *boolable = new vector<string>{"isOn"};
+
+void tests(Config& config, Devices& devices, Dirigera& dirigera) {
+    //nlohmann::json lamp = dirigera.getDevice(devices.getDevice("Desk Lamp"));
+    //cout << lamp.dump(4) << endl;
+    //dirigera.identifyDevice(devices.getDevice("Desk Lamp"));
+    dirigera.setDeviceAttribute(devices.getDevice("Desk Lamp"), "isOn", true);
+}
 
 int main(int argc, char** argv) {
     try {
@@ -35,8 +47,6 @@ int main(int argc, char** argv) {
             }
         }
 
-        //cout << config.toString() << endl;
-        //cout << devices.toString() << endl;
         int pingTimeout = argc <= 1 ? 1000 : 500;
         bool connection = Authenticator::TestConnection(config.ip, pingTimeout);
         while (!connection) {
@@ -68,8 +78,34 @@ int main(int argc, char** argv) {
                     continue;
             }
         }
-        string token = Authenticator::GetToken(config.ip, stoi(config.port), config.authCode, config.verification, 0);
-        cout << "Token: " << token << endl;
+        if (config.token.empty()) {
+            config.verification = Authenticator::GetVerification();
+            config.authCode = Authenticator::Authorise(config.ip, stoi(config.port), config.verification);
+            config.token = Authenticator::GetToken(config.ip, stoi(config.port), config.authCode, config.verification, 0);
+            config.Save();
+        }
+        Dirigera dirigera(config.ip, config.port, config.token);
+        nlohmann::json devicesJson = dirigera.getDevices();
+
+        for (auto &device : devicesJson) {
+            devices.addDevice(device["attributes"]["customName"], device["id"]);
+        }
+        if (!devices.isEmpty()) {
+            devices.Save();
+        }
+        cout << argc << endl;
+        if (argc > 1) {
+            string deviceName = argv[1];
+            string attribute = argv[2];
+            string value = argv[3];
+            cout << "Setting " << deviceName << " " << attribute << " to " << value << endl;
+            if (find(boolable->begin(), boolable->end(), attribute) != boolable->end()) {
+                dirigera.setDeviceAttribute(devices.getDevice(deviceName), attribute, bool(stoi(value)));
+            } else {
+                dirigera.setDeviceAttribute(devices.getDevice(deviceName), attribute, value);
+            }
+        }
+        // tests(config, devices, dirigera);
     } catch (std::exception &e) {
         cout << e.what() << endl;
     }
